4733adefeab929979e0281aa65ca68d729eb717e
Brad Abrahams <email@bradabrahams.co.uk>
2013-08-25 18:50:55 +0100

    copy tweak

--- orbital.py
+++ orbital.py
@@ -141,7 +141,7 @@ message = """From: %s <%s>
 To: %s
 MIME-Version: 1.0
 Content-type: text/html
-Subject: New Orbital comic releases on Wednesday %s
+Subject: New Orbital comic releases, Wednesday %s
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
